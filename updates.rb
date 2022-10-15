f5d7bd782814719bfb52268098895e1b381c4034
GroovyWizard <lucasfelipe260702@gmail.com>
2022-10-15 20:28:49 -0300

    Added support for multiple distributions

--- updates.rb
+++ updates.rb
@@ -1,12 +1,29 @@
-def check_for_updates()
-  result = %x( dnf check-update | wc -l )
-  message = result.to_s()
-  if(result.to_i() > 1)
-    return "  " + message
-  end
+$distro_info = ARGV[0]
 
-  return "  0" 
+def distro_update_command(distro)
+  case distro
+  when 'fedora'
+    `dnf check-update | wc -l`
+  when 'arch'
+    `checkupdates | wc -l`
+  end
 end
 
-puts check_for_updates()
+def distro_icon(distro)
+  case distro
+  when 'fedora'
+    '  '
+  when 'arch'
+    '  '
+  end
+end
 
+def check_for_updates
+  result = distro_update_command($distro_info)
+  message = result.to_s
+  return distro_icon($distro_info) + message if result.to_i > 1
+
+  distro_icon($distro_info) + '0'
+end
+
+puts check_for_updates
